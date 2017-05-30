a95a0fe87ab8114c9ef44fa15bda832bdca4ebb9
NahuelOlgiati <nahuelolgiati@hotmail.com>
2017-05-30 11:31:15 -0300

    multi movie search

--- src/app/shared/service/movie.service.ts
+++ src/app/shared/service/movie.service.ts
@@ -1,51 +1,53 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Http, Response } from '@angular/http';
+import { LanguageService } from './language.service';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class MovieService {
-  //private url = 'http://api.allocine.fr/rest/v3/';
-  //private partner = 'YW5kcm9pZC12Mg'
-  //private partner = '000042532791';
+
+  private language: string;
   private url = 'https://api.themoviedb.org/3/';
   private partner = '431bc17da732dfb3be082e58f7a5cf27';
 
-  constructor(private http: Http) { }
+  constructor(private http: Http, private languageService: LanguageService) {
+    this.languageService.languageChanged.subscribe(language => this.language = language);
+  }
 
   getSearch(name: string, page: number) {
-    return this.http.get(this.url + 'search/movie?api_key=' + this.partner + '&language=fr' + '&query=' + name + '&page=' + page)
+    return this.http.get(this.url + 'search/movie?api_key=' + this.partner + '&language=' + this.language + '&query=' + name + '&page=' + page)
       .map((res: Response) => res.json());
   }
 
   getNowPlaying(page: number) {
-    return this.http.get(this.url + 'movie/now_playing?api_key=' + this.partner + '&language=fr' + '&page=' + page)
+    return this.http.get(this.url + 'movie/now_playing?api_key=' + this.partner + '&language=' + this.language + '&page=' + page)
       .map((res: Response) => res.json());
   }
 
 
   getDetails(code: number) {
-    return this.http.get(this.url + 'movie/' + code + '?api_key=' + this.partner + '&language=fr')
+    return this.http.get(this.url + 'movie/' + code + '?api_key=' + this.partner + '&language=' + this.language)
       .map((res: Response) => res.json());
   }
 
   getDiscover(page: number) {
-    return this.http.get(this.url + 'discover/movie?api_key=' + this.partner + '&language=fr&sort_by=popularity.desc&include_video=false&page=' + page)
+    return this.http.get(this.url + 'discover/movie?api_key=' + this.partner + '&language=' + this.language + '&sort_by=popularity.desc&include_video=false&page=' + page)
       .map((res: Response) => res.json());
   }
 
   getVideo(code: number) {
-    return this.http.get(this.url + 'movie/' + code + '/videos?api_key=' + this.partner + '&language=fr-FR')
+    return this.http.get(this.url + 'movie/' + code + '/videos?api_key=' + this.partner + '&language=' + this.language)
       .map((res: Response) => res.json());
   }
 
   getSimilarMovies(code: number) {
-    return this.http.get(this.url + 'movie/' + code + '/similar?api_key=' + this.partner + '&language=fr')
+    return this.http.get(this.url + 'movie/' + code + '/similar?api_key=' + this.partner + '&language=' + this.language)
       .map((res: Response) => res.json());
   }
 
   getUpComing(page: number) {
-    return this.http.get(this.url + 'movie/upcoming?api_key=' + this.partner + '&language=fr&page=' + page)
+    return this.http.get(this.url + 'movie/upcoming?api_key=' + this.partner + '&language=' + this.language + '&page=' + page)
       .map((res: Response) => res.json());
   }
 
